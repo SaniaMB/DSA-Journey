3921044bddf5a6e16f9405a9370b849c4454b9ab
SaniaMB <saniabhandari05@gmail.com>
2025-05-20 13:55:48 +0530

    Added ReverseString.java under Basics

---
+++ Basics/ReverseString.java
@@ -0,0 +1,25 @@
+public class ReverseStringManual {
+    public static void main(String[] args) {
+        String input = "hello";
+        String reversed = "";
+
+        // Loop from last character to first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i); // append character one by one
+        }
+
+        System.out.println(reversed);
+    }
+}
+/*
+// Problem: Reverse a String
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "hello";
+        String reversed = new StringBuilder(input).reverse().toString();
+        System.out.println(reversed);
+    }
+}
+
+ */
