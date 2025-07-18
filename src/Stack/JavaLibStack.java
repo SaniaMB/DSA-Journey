d00191091798c82a36e96903762bc3a241735676
SaniaMB <saniabhandari05@gmail.com>
2025-07-18 20:30:47 +0530

    Stack Implemetation

---
+++ src/Stack/JavaLibStack.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import java.util.Stack;
+
+public class JavaLibStack {
+    public static void main(String[] args) {
+      Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
---
+++ src/Stack/StackArrayList.java
@@ -0,0 +1,52 @@
+package Stack;
+
+import java.util.ArrayList;
+
+public class StackArrayList {
+    // STACK IMPLEMENTATION USING ARRAYLIST
+
+    static class Stack{
+       public static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        public static void push(int data){
+            list.add(data);
+        }
+
+        public static int pop(){
+            if(isEmpty()) return -1;
+
+           int top =  list.get(list.size()-1);   //getLast
+           list.removeLast(); //  list.remove(list.size()-1);
+           return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()) return -1;
+
+            int top =  list.get(list.size()-1);
+
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+        System.out.println(s.peek());
+
+    }
+}
