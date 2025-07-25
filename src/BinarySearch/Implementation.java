efc719c3cb5cf0a436f73483a4542951231632b3
SaniaMB <saniabhandari05@gmail.com>
2025-07-25 21:47:33 +0530

    Binary Search Implementation

---
+++ src/BinarySearch/Implementation.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class Implementation {
+    public static int linearSearch(int[] nums , int target){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target) return i;
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] nums , int target,int left,int right){
+
+        int mid = (left + right)/2;
+
+        if(nums[mid] == target) return mid;
+       if(target > nums[mid]) return binarySearch(nums,target,mid + 1,right);
+       if(target < nums[mid]) return binarySearch(nums,target,left,mid - 1);
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,6,8,9,34,55,67,78};
+        int target = 6;
+
+        System.out.println(linearSearch(arr,target));
+        System.out.println(binarySearch(arr,target,0,arr.length-1));
+    }
+}
